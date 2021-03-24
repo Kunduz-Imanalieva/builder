f32e1970aef4b6478e3844a058adb6e5612f63af
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-24 12:57:25 +0600

    open and close drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,18 @@
-import Drawer from "../Drawer/Drawer";
-import Toolbar from "../Toolbar/Toolbar";
+import { useState } from "react";
 import classes from "./Layout.module.css";
+import Toolbar from "../Toolbar/Toolbar";
+import Drawer from "../Drawer/Drawer";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <div className = {classes.Layout}>
-     <Toolbar/>
-     <Drawer/>
-     {children}
+    <div className={classes.Layout}>
+      <Toolbar openDrawer={() => setDrawerOpen(true)} />
+      <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
+      {children}
     </div>
   );
 }
-
+ 
 export default Layout;
