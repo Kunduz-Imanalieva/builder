78aa4dced5c6ed83d7b49cc4a604872ddaf043d3
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-27 21:54:07 +0600

    created add and remove ingredients Functions

--- src/components/SaladBuilder/SaladBuilder.js
+++ src/components/SaladBuilder/SaladBuilder.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import classes from "./SaladBuilder.module.css";
 import SaladControls from "./SaladControls/SaladControls";
 import SaladPreview from "./SaladPreview/SaladPreview";
 
 const SaladBuilder = () => {
-    const ingredients ={
+    const [ingredients, setIngredients ]= useState({
         tomato: 1,
         avocado: 1,
         cucumber: 1,
-    };
+    });
+
+
+    function addIngredient(type) {
+        const newIngredients = { ...ingredients};
+        newIngredients[type]++;
+        setIngredients(newIngredients)
+    }
+
+
+    function removeIngredient(type) {
+        const newIngredients = { ...ingredients};
+        newIngredients[type]--;
+        setIngredients(newIngredients)
+    }
 
     return ( <div className={classes.SaladBuilder}>
         <SaladPreview ingredients={ingredients}/>
-        <SaladControls/>
+        <SaladControls ingredients={ingredients}
+        addIngredient={addIngredient}
+         removeIngredient={removeIngredient}/>
+         
     </div>
      );
 }
