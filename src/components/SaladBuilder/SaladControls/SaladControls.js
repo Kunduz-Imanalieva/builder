c1c391c1648b8f6c4863e6c327b186a6bcb64989
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-27 21:52:39 +0600

    imported salad control with add and remove ingredients

--- src/components/SaladBuilder/SaladControls/SaladControls.js
+++ src/components/SaladBuilder/SaladControls/SaladControls.js
@@ -1,8 +1,16 @@
+import SaladControl from "./SaladControl/SaladControl";
 import classes from "./SaladControls.module.css";
 
-const SaladControls = () => {
-    return ( <div className={classes.SaladControls}>
+const SaladControls = ({ingredients, addIngredient, removeIngredient}) => {
+    let type = {ingredients}
+    const results = [];
+    for (const ingredient in ingredients) {
+        results.push( <SaladControl type={ingredient} addIngredient={addIngredient} removeIngredient={removeIngredient} />);
+    }
 
+    return ( 
+    <div className={classes.SaladControls}>
+       {results}
     </div> );
 }
  
