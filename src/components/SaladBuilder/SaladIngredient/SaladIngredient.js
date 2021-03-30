de2180a09363f8dbe64e132fbd1a5afb8bff8899
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-30 20:45:59 +0600

    imported images to Salad Ingredients

--- src/components/SaladBuilder/SaladIngredient/SaladIngredient.js
+++ src/components/SaladBuilder/SaladIngredient/SaladIngredient.js
@@ -1,6 +1,20 @@
+
+import classes from "./SaladIngredient.module.css";
+import cucumberBackground from "../../../images/cucumber.svg";
+import lettuceBackground from "../../../images/lettuce.svg";
+import spinachBackground from "../../../images/spinach.svg";
+
+
 const SaladIngredient = ({type}) => {
+    const types = {
+        cucumber: {cucumberBackground: `url(${cucumberBackground})`, width: "35px", height: "35px"},
+        lettuce: {lettuceBackground: `url(${lettuceBackground})`, width: "35px", height: "35px"},
+        spinach: {spinachBackground: `url(${spinachBackground})`, width: "35px", height: "35px"},
+    }
+
+    
     return ( 
-    <div>{type}</div> 
+    <div className={classes.SaladIngredient}>{type}</div> 
     );
 }
  
