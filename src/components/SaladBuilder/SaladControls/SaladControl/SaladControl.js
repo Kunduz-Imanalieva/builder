50c1ada1241234ac0bebc934c3bad15c488d1dab
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-30 21:23:48 +0600

    imported SaladIngredient and added classes to buttons

--- src/components/SaladBuilder/SaladControls/SaladControl/SaladControl.js
+++ src/components/SaladBuilder/SaladControls/SaladControl/SaladControl.js
@@ -1,11 +1,16 @@
+import SaladIngredient from "../../SaladIngredient/SaladIngredient";
 import classes from "./SaladControl.module.css";
 
 const SaladControl = ({type, addIngredient, removeIngredient}) => {
-    return ( <div className={classes.SaladControl}>
-    <button onClick={() => addIngredient(type)}>+</button>
-    
-    <button onClick={() => removeIngredient(type)}>-</button>
-    </div> );
+    return ( 
+    <div className={classes.SaladControl}>
+    <button className={classes.more} onClick={() => addIngredient(type)}>+</button>
+    <div className={classes.ingredient}>
+      <SaladIngredient type={type} fixed />
+    </div>
+    <button className={classes.less} onClick={() => removeIngredient(type)}>-</button>
+    </div> 
+    );
 }
  
 export default SaladControl;
