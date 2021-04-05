352b2d12cc85893a194a7ea1c97f9f2a15e5c136
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-05 12:00:18 +0600

    added random position and random rotation logic

--- src/components/SaladBuilder/SaladIngredient/SaladIngredient.js
+++ src/components/SaladBuilder/SaladIngredient/SaladIngredient.js
@@ -1,20 +1,49 @@
-
+import React from "react";
 import classes from "./SaladIngredient.module.css";
 import cucumberBackground from "../../../images/cucumber.svg";
 import lettuceBackground from "../../../images/lettuce.svg";
 import spinachBackground from "../../../images/spinach.svg";
 
 
-const SaladIngredient = ({type}) => {
+const SaladIngredient = ({type, fixed}) => {
     const types = {
-        cucumber: {cucumberBackground: `url(${cucumberBackground})`, width: "35px", height: "35px"},
-        lettuce: {lettuceBackground: `url(${lettuceBackground})`, width: "35px", height: "35px"},
-        spinach: {spinachBackground: `url(${spinachBackground})`, width: "35px", height: "35px"},
+        cucumber: {backgroundImage: `url(${cucumberBackground})`, width: "35px", height: "35px"},
+        lettuce: {backgroundImage: `url(${lettuceBackground})`, width: "35px", height: "35px"},
+        spinach: {backgroundImage: `url(${spinachBackground})`, width: "35px", height: "35px"},
     }
 
+
+    function getPosition(ingredientWidth) {
+        const saladDiameter = 380;
+        const saladRadius = saladDiameter / 2;
+        const ingredientRadius = parseInt(ingredientWidth) / 2;
+    
+        const ingredientTop = Math.round(Math.random() * saladDiameter);
+        const ingredientLeft = Math.round(Math.random() * saladDiameter);
+    
+        const distance = Math.sqrt(
+          Math.pow(ingredientTop - saladRadius, 2) + Math.pow(ingredientLeft - saladRadius, 2)
+        ) + ingredientRadius;
+    
+        return distance < saladRadius
+          ? {
+            top: ingredientTop - ingredientRadius,
+            left: ingredientLeft - ingredientRadius
+          }
+          : getPosition(ingredientWidth);
+      }
+    
+      // Get random position for this ingredient.
+      if (!fixed) {
+        const position = getPosition(types[type].width);
+        types[type].top = position.top + "px";
+        types[type].left = position.left + "px";
+      }
+      // Get random rotation for this ingredient.
+      types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
     
     return ( 
-    <div className={classes.SaladIngredient}>{type}</div> 
+    <div className={classes.SaladIngredient} style={types[type]}></div> 
     );
 }
  
