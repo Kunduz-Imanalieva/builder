a76b86baec57d879e1c7bb394882741e762e8382
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-05 15:41:44 +0600

    added price prop

--- src/components/SaladBuilder/SaladPreview/SaladPreview.js
+++ src/components/SaladBuilder/SaladPreview/SaladPreview.js
@@ -1,7 +1,7 @@
 import SaladIngredient from "../SaladIngredient/SaladIngredient";
 import classes from "./SaladPreview.module.css";
 
-const SaladPreview = ({ingredients}) => {
+const SaladPreview = ({ingredients, price}) => {
     const result = [];
 
     for (const ingredient in ingredients) {
@@ -9,13 +9,17 @@ const SaladPreview = ({ingredients}) => {
            result.push(<SaladIngredient key={ingredient + i} type={ingredient}/>)
        }
     }
-    return ( <div className={classes.SaladPreview}>
-      <div 
-      className={classes.ingredients}>
-      {result}
+    return (
+      <div className={classes.SaladPreview}>
+        <div className={classes.salad}>
+          <div
+            className={classes.ingredients}>
+            {result}
+          </div>
+        </div>
+        <div className={classes.price}>{price} som</div>
       </div>
-    </div> 
     );
-}
+  }
  
 export default SaladPreview;
