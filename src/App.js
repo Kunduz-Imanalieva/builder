38860315ce065e0d960742bc0fc81221a5c8d934
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-16 14:47:35 +0600

    added and used checkout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Layout from "./components/Layout/Layout";
 import "./App.css";
 import SaladBuilder from "./components/SaladBuilder/SaladBuilder";
+import Checkout from "./components/Checkout/Checkout";
+import { Redirect, Route, Switch } from "react-router";
 function App() {
   return (
     <div className="App">
       <Layout>
-        <SaladBuilder />
+      <Switch>
+          <Route path="/" component={SaladBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
       </Layout>
     </div>
   );
