64f1b76a969723f95f6a7ec78484eac227efefaf
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-05-07 15:01:28 +0600

    load data for checkout orders from firebase

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,22 +1,41 @@
-
+import classes from "./Checkout.module.css";
 import SaladPreview from "../SaladBuilder/SaladPreview/SaladPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Checkout = ({ history }) => {
+  const ingredients = useSelector((state) => state.ingredients);
+  const price = useSelector((state) => state.price);
   function cancelCallback() {
-    history.replace('/');
+    history.replace("/");
+  }
+
+  function submitCallback(event) {
+    const data = new FormData(event.target);
+    axios
+      .post("https://builder-e08b0-default-rtdb.firebaseio.com/orders.json", {
+        name: data.get("name"),
+        address: data.get("address"),
+        phone: data.get("phone"),
+        ingredients: ingredients,
+        price: price,
+      })
+      .then((response) => {
+        history.replace("/");
+      });
+    event.preventDefault();
   }
 
   return (
-    <div>
-        <SaladPreview ingredients={{
-        tomato: 5, 
-        cucumber: 10,
-        lettuce: 20,
-      }} price={150} />
-      <CheckoutForm />
+    <div className={classes.Checkout}>
+      <SaladPreview ingredients={ingredients} price={price} />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback}
+      />
     </div>
   );
-}
- 
+};
+
 export default Checkout;
