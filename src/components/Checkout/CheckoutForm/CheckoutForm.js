414e534ee938e239d204a0c74fec2032249e82b1
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-05-10 15:01:43 +0600

    some styles to checkout and checkout form

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -1,19 +1,20 @@
+import classes from "./CheckoutForm.module.css";
 import Button from "../../UI/Button/Button"
 
 const CheckoutForm = () => {
   return (
-    <form>
+    <form className={classes.CheckoutForm}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" required />
       </div>
       <div>
         <label htmlFor="address">Address</label>
-        <input type="text" name="address" id="address" />
+        <input type="text" name="address" id="address" required/>
       </div>
       <div>
         <label htmlFor="phone">Phone</label>
-        <input type="text" name="phone" id="phone" />
+        <input type="text" name="phone" id="phone" required pattern="0[0-9]{9}"/>
       </div>
       <Button>Checkout</Button>
       <Button>Cancel</Button>
