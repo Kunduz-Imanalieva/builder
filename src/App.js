20fc309a1e548e13aa90800e15dc37a043f273d8
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-05-19 15:55:52 +0600

    Added isAuthenticated to App.js and Nav.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,41 @@ import { Redirect, Route, Switch } from "react-router";
 import Orders from "./components/Orders/Orders";
 import Auth from "./components/Auth/Auth";
 import Logout from "./components/Logout/Logout";
-function App() {
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { restore } from "./store/actions/auth";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  useEffect(() => restore(dispatch), [dispatch]);
+
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact component={SaladBuilder} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact component={SaladBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
   return (
     <div className="App">
       <Layout>
-      <Switch>
-          <Route path="/" component={SaladBuilder} exact />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Redirect to="/" />
-        </Switch>
+      {routesOutput}
       </Layout>
     </div>
   );
