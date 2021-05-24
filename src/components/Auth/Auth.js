4567e96dfdc5bf8600b86d7027f24c5c233ae163
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-05-24 15:25:47 +0600

    Stylized the Login page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -9,18 +9,18 @@ import { Redirect, useLocation } from "react-router-dom";
 
 export default withAxios(() => {
   const dispatch = useDispatch();
-  const [ method, setMethod ] = useState("signin");
-  const { loading, error, token } = useSelector(state => state.auth);
+  const [method, setMethod] = useState("signin");
+  const { loading, error, token } = useSelector((state) => state.auth);
   const location = useLocation();
 
   const formSubmitted = (event) => {
     start(dispatch);
 
     const data = new FormData(event.target);
-    auth(dispatch, method, data.get('email'), data.get('password'));
+    auth(dispatch, method, data.get("email"), data.get("password"));
 
     event.preventDefault();
-  }
+  };
 
   let formOutput = "Loading...";
   if (!loading) {
@@ -28,9 +28,21 @@ export default withAxios(() => {
       <form onSubmit={formSubmitted}>
         <h1>Welcome</h1>
         <input type="email" placeholder="E-mail" name="email" required />
-        <input type="password" placeholder="Password" name="password" required minLength="6" />
-        <Button click={() => setMethod('signin')} green>Sign in</Button>
-        <Button click={() => setMethod('signup')} red>Sign up</Button>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          minLength="6"
+        />
+        <div className={classes.AuthBtns}>
+          <Button click={() => setMethod("signin")} green>
+            Sign in
+          </Button>
+          <Button click={() => setMethod("signup")} red>
+            Sign up
+          </Button>
+        </div>
       </form>
     );
   }
@@ -40,7 +52,7 @@ export default withAxios(() => {
     errorOutput = <h4 className={classes.error}>{error.message}</h4>;
   }
 
-  const [,redirect] = location.search.split('?');
+  const [, redirect] = location.search.split("?");
   let redirectOutput = null;
   if (token !== null) {
     redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
@@ -53,4 +65,4 @@ export default withAxios(() => {
       {redirectOutput}
     </div>
   );
-}, axios); 
+}, axios);
