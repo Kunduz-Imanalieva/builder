cddbbc0f36af24f394ad73e7ca8d3dd843411173
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-05-24 15:03:02 +0600

    some style changes

--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -5,8 +5,13 @@ const initialState = [];
 const orders = (state = initialState, action) => {
   switch (action.type) {
     case SET_ORDERS:
-      return [ ...action.orders ];
-
+      return Object.keys(action.data).map(id => {
+        return {
+          ...action.data[id],
+          id: id,
+        };
+      });;
+  
     default:
       break;
   }
